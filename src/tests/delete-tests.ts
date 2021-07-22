49250a46ccc58f392de00ff0d962c6747685a997
Christopher Jennings <25697451+CGJennings@users.noreply.github.com>
2021-07-22 16:10:58 -0400

    add delete() tests

---
+++ src/tests/delete-tests.ts
@@ -0,0 +1,52 @@
+import { TernaryStringSet } from "../index";
+import { load } from "./word-list-loader";
+
+let tree: TernaryStringSet;
+const words = load("short-english");
+
+beforeEach(() => {
+    tree = new TernaryStringSet();
+});
+
+test("Delete non-member", () => {
+    expect(tree.size).toBe(0);
+    tree.add("dog");
+    expect(tree.size).toBe(1);
+    expect(tree.has("cat")).toBe(false);
+    expect(tree.delete("cat")).toBe(false);
+    expect(tree.size).toBe(1);
+});
+
+test("Delete member", () => {
+    expect(tree.size).toBe(0);
+    tree.add("dog");
+    expect(tree.size).toBe(1);
+    expect(tree.has("dog")).toBe(true);
+    expect(tree.delete("dog")).toBe(true);
+    expect(tree.size).toBe(0);
+});
+
+test("Delete multiple", () => {
+    tree.addAll(words);
+    let size = tree.size;
+    const randomOrder = shuffle([...words]);
+
+    for (const w of randomOrder) {
+        expect(tree.size).toBe(size--);
+        expect(tree.has(w)).toBe(true);
+        expect(tree.delete(w)).toBe(true);
+        expect(tree.has(w)).toBe(false);
+    }
+    expect(tree.size).toBe(0);
+});
+
+function shuffle<T>(array: T[]): T[] {
+    let i = array.length, toSwap;
+    while (i > 0) {
+        toSwap = Math.floor(Math.random() * i);
+        const temp = array[--i];
+        array[i] = array[toSwap];
+        array[toSwap] = temp;
+    }
+    return array;
+}
