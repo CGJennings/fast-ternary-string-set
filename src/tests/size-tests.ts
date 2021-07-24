887aa3e8ac47a1696921345181b506ce90020f9c
Christopher Jennings <25697451+CGJennings@users.noreply.github.com>
2021-07-24 12:06:45 -0400

    add size tests

---
+++ src/tests/size-tests.ts
@@ -0,0 +1,54 @@
+import { TernaryStringSet } from "../index";
+import { load } from "./word-list-loader";
+
+let set: TernaryStringSet;
+const words = load("short-english");
+
+beforeEach(() => {
+  set = new TernaryStringSet();
+});
+
+test("Not double counted", () => {
+    expect(set.size).toBe(0);
+    set.add("peach");
+    expect(set.size).toBe(1);
+    set.add("peach");
+    expect(set.size).toBe(1);
+});
+
+test("Not double deleted", () => {
+    expect(set.size).toBe(0);
+    set.add("peach");
+    expect(set.size).toBe(1);
+    set.delete("peach");
+    expect(set.size).toBe(0);
+    set.delete("peach");
+    expect(set.size).toBe(0);
+});
+
+test("Empty string", () => {
+    expect(set.size).toBe(0);
+    set.add("");
+    expect(set.size).toBe(1);
+    set.add("");
+    expect(set.size).toBe(1);
+    set.delete("");
+    expect(set.size).toBe(0);
+    set.delete("");
+    expect(set.size).toBe(0);
+});
+
+test("Accurate for many words/addAll", () => {
+    expect(set.size).toBe(0);
+    set.addAll(words);
+    expect(set.size).toBe(words.length);
+    let i=0;
+    for(const el of set) {
+        set.delete(el);
+        expect(set.size).toBe(words.length - ++i);
+    }
+    for (const el of words) {
+        set.delete(el);
+        expect(set.size).toBe(0);
+    }
+});
